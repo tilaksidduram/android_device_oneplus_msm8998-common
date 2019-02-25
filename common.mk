80fccb5726bb9f5926a5ac2dbecefa2b1f7c6d71
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-02-25 17:58:19 +0100

    msm8998-common: build versioned vndk
    Change-Id: I0a3d40619819e45f8193d20a632fe36714f3d057
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -168,7 +168,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    libstdc++.vendor \
     GoogleCamera \
     Snap2
 
@@ -484,12 +483,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
+# Update this list with what each blob is actually for
+# libstdc++: camera.msm8998
 PRODUCT_PACKAGES += \
+    libstdc++.vendor \
     vndk_package
-
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk-sp
 
 # VR
 PRODUCT_PACKAGES += \
