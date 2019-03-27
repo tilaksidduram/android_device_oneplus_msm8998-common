375720fac21d52ecb66341fd812f3a5ef4ea9839
Tilak <tilaksidduram@gmail.com>
2019-03-27 22:50:59 +0530

    msm8998-common: Add freefrom Multiwindow support

--- common.mk
+++ common.mk
@@ -97,6 +97,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
 
