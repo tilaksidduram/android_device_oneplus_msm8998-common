803e3dbd0277c2e63835798ba3f1bcb263de4b34
Pranav Vashi <neobuddy89@gmail.com>
2019-04-21 01:00:04 +0530

    msm8998: Switch to clang 9.0.3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := 9.0.2
+TARGET_KERNEL_CLANG_VERSION := 9.0.3
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
