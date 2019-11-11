e22e8f31814e852a4a49f078f8ad2acdeb0e7acd
Tilak <tilaksidduram@gmail.com>
2019-11-11 20:49:05 +0530

    msm8998-common: Set TARGET_USES_NON_LEGACY_POWERHAL to true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -267,6 +267,7 @@ TARGET_USES_MKE2FS := true
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_USES_NON_LEGACY_POWERHAL := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
