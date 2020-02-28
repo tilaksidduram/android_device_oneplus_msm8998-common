5387640dc00eff4984e1709b3d82cb61c94d9056
Tilak <tilaksidduram@gmail.com>
2020-02-28 19:10:02 +0530

    Revert "msm8998-common: Add  OnePlus Gallery v3.7.24"
    This reverts commit 934c95182189e2e5cb69d9b31ddbb5ae1f71a3c6.

--- common.mk
+++ common.mk
@@ -338,10 +338,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Prebuilts
-PRODUCT_PACKAGES += \
-    OnePlusGallery
-
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
